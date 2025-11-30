6f23dc449466ac10b7faff919dffa5b286b7ab5c
Jun3671 <wnsgur3027@naver.com>
2025-11-30 20:22:05 +0900

    환경설정

---
+++ learnhub-project/src/main/java/org/example/learnhubproject/LearnhubProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.learnhubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnhubProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnhubProjectApplication.class, args);
+    }
+
+}
---
+++ learnhub-project/src/test/java/org/example/learnhubproject/LearnhubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.learnhubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnhubProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
